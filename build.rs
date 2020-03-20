23ff9947d363266728bb4a2378b6d2bf8e43990e
Jack O'Connor <oconnor663@gmail.com>
2020-03-20 17:10:19 -0400

    copy wrapper code from blakek2_simd benchmarks
    https://github.com/oconnor663/blake2_simd/tree/master/benches/kangarootwelve

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    // For 32-bit targets, replace this with "generic32".
+    let target = "generic64";
+    let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
+    let k12_dir = manifest_dir.join("XKCP-K12");
+    let build_dir = k12_dir.join(format!("bin/{}", target));
+    let build_status = Command::new("make")
+        .arg(format!("{}/libk12.a", target))
+        .current_dir(&k12_dir)
+        .status()
+        .unwrap();
+    assert!(build_status.success());
+    println!("cargo:rustc-link-search={}", build_dir.to_str().unwrap());
+    println!("cargo:rustc-link-lib=static=k12");
+
+    // Note that because this build relies on Make, it's not good at noticing
+    // changes. In general you'll need to `cargo clean` whenever you change
+    // something.
+}
