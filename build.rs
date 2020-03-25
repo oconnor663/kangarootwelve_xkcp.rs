ccdf78296e4adfc31084b1dfa65b480593b31881
Jack O'Connor <oconnor663@gmail.com>
2020-03-25 19:36:50 -0400

    stop using the upstream Makefile system
    It looks like it shouldn't be too hard to use a pure build.rs build with
    the usual `cc` crate. If this works, it'll get rid of the build
    dependency on xsltproc.

--- build.rs
+++ build.rs
@@ -1,28 +1,45 @@
 use std::env;
-use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
-    let k12_target = match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
-        "64" => "generic64",
-        "32" => "generic32",
+    let mut build = cc::Build::new();
+    build.include("XKCP-K12/lib");
+    build.file("XKCP-K12/lib/KangarooTwelve.c");
+    let k12_target;
+    match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
+        "64" => {
+            build.include("XKCP-K12/lib/Optimized64");
+            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-opt64.c");
+            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX2.s");
+            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX512.s");
+
+            let mut ssse3_build = cc::Build::new();
+            ssse3_build.flag("-mssse3");
+            ssse3_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-SSSE3.c");
+            ssse3_build.compile("k12_ssse3");
+            ssse3_build.compile("k12_ssse3");
+
+            let mut avx2_build = cc::Build::new();
+            avx2_build.flag("-mavx2");
+            avx2_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX2.c");
+            avx2_build.compile("k12_avx2");
+
+            let mut avx512_build = cc::Build::new();
+            avx512_build.flag("-mavx512f");
+            avx512_build.flag("-mavx512vl");
+            avx512_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX512.c");
+            avx512_build.compile("k12_avx512");
+
+            k12_target = "generic64";
+        }
+        "32" => {
+            build.include("XKCP-K12/lib/Inplace32BI");
+            build.file("XKCP-K12/lib/Inplace32BI/KeccakP-1600-inplace32BI.c");
+            // The 32-bit code includes a switch with intentional fallthrough.
+            build.flag("-Wno-implicit-fallthrough");
+            k12_target = "generic32";
+        }
         x => panic!("unexpected target pointer width: {}", x),
     };
+    build.compile("k12");
     println!("cargo:rustc-cfg=k12_target=\"{}\"", k12_target);
-
-    let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let k12_dir = manifest_dir.join("XKCP-K12");
-    let build_dir = k12_dir.join(format!("bin/{}", k12_target));
-    let build_status = Command::new("make")
-        .arg(format!("{}/libk12.a", k12_target))
-        .current_dir(&k12_dir)
-        .status()
-        .unwrap();
-    assert!(build_status.success());
-    println!("cargo:rustc-link-search={}", build_dir.to_str().unwrap());
-    println!("cargo:rustc-link-lib=static=k12");
-
-    // Note that because this build relies on Make, it's not good at noticing
-    // changes. In general you'll need to `cargo clean` whenever you change
-    // something.
 }
