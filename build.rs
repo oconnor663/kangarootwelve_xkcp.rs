f9e3b6214f8c58be405a180d679be20daf860d9e
Jack O'Connor <oconnor663@gmail.com>
2020-03-26 13:20:49 -0400

    only build the generic64 target on x86_64

--- build.rs
+++ build.rs
@@ -5,44 +5,40 @@ fn main() {
     build.include("XKCP-K12/lib");
     build.file("XKCP-K12/lib/KangarooTwelve.c");
 
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let k12_target;
-    match env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap().as_str() {
-        "64" => {
-            build.include("XKCP-K12/lib/Optimized64");
-            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-opt64.c");
-            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX2.s");
-            build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX512.s");
+    if target_arch == "x86_64" {
+        k12_target = "generic64";
+        build.include("XKCP-K12/lib/Optimized64");
+        build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-opt64.c");
+        build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX2.s");
+        build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-AVX512.s");
 
-            let mut ssse3_build = cc::Build::new();
-            ssse3_build.flag("-mssse3");
-            ssse3_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-SSSE3.c");
-            ssse3_build.compile("k12_ssse3");
-            ssse3_build.compile("k12_ssse3");
+        let mut ssse3_build = cc::Build::new();
+        ssse3_build.flag("-mssse3");
+        ssse3_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-SSSE3.c");
+        ssse3_build.compile("k12_ssse3");
+        ssse3_build.compile("k12_ssse3");
 
-            let mut avx2_build = cc::Build::new();
-            avx2_build.flag("-mavx2");
-            avx2_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX2.c");
-            avx2_build.compile("k12_avx2");
+        let mut avx2_build = cc::Build::new();
+        avx2_build.flag("-mavx2");
+        avx2_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX2.c");
+        avx2_build.compile("k12_avx2");
 
-            let mut avx512_build = cc::Build::new();
-            avx512_build.flag("-mavx512f");
-            avx512_build.flag("-mavx512vl");
-            avx512_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX512.c");
-            avx512_build.compile("k12_avx512");
-
-            k12_target = "generic64";
-        }
-        "32" => {
-            build.include("XKCP-K12/lib/Inplace32BI");
-            build.file("XKCP-K12/lib/Inplace32BI/KeccakP-1600-inplace32BI.c");
-            // The 32-bit code includes a switch with intentional fallthrough.
-            build.flag("-Wno-implicit-fallthrough");
-            // The 32-bit code has some unused variables.
-            build.flag("-Wno-unused-variable");
-            k12_target = "generic32";
-        }
-        x => panic!("unexpected target pointer width: {}", x),
-    };
+        let mut avx512_build = cc::Build::new();
+        avx512_build.flag("-mavx512f");
+        avx512_build.flag("-mavx512vl");
+        avx512_build.file("XKCP-K12/lib/Optimized64/KeccakP-1600-timesN-AVX512.c");
+        avx512_build.compile("k12_avx512");
+    } else {
+        k12_target = "generic32";
+        build.include("XKCP-K12/lib/Inplace32BI");
+        build.file("XKCP-K12/lib/Inplace32BI/KeccakP-1600-inplace32BI.c");
+        // The 32-bit code includes a switch with intentional fallthrough.
+        build.flag("-Wno-implicit-fallthrough");
+        // The 32-bit code has some unused variables.
+        build.flag("-Wno-unused-variable");
+    }
     println!("cargo:rustc-cfg=k12_target=\"{}\"", k12_target);
 
     // brg_endian.h tries to detect the target endianness, but it fails on e.g.
