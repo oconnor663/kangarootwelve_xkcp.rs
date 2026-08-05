93883adf6ab02eec9030549c24a552a9fd1a48fc
Jack O'Connor <oconnor663@gmail.com>
2026-08-05 16:33:39 -0700

    Codex: port the new `maybe_mmap_file` function from `blake3`

--- k12sum/src/main.rs
+++ k12sum/src/main.rs
@@ -3,10 +3,11 @@ use clap::Parser;
 use std::cmp;
 use std::fs::File;
 use std::io;
-use std::io::prelude::*;
+use std::io::{BufRead, Read};
 use std::path::{Path, PathBuf};
 
 const NAME: &str = "k12sum";
+const MINIMUM_MMAP_SIZE: u64 = 16 * 1024; // 16 KiB
 
 #[derive(Parser)]
 #[command(version, max_term_width(100))]
@@ -66,7 +67,7 @@ struct Args {
 }
 
 enum Input {
-    Mmap(io::Cursor<memmap::Mmap>),
+    Mmap(io::Cursor<memmap2::Mmap>),
     File(File),
     Stdin,
 }
@@ -79,14 +80,10 @@ impl Input {
         if path == Path::new("-") {
             return Ok(Self::Stdin);
         }
-        let file = File::open(path)?;
+        let mut file = File::open(path)?;
         if !args.no_mmap {
-            // Heuristic: memory mapping isn't worth it below 16 KiB.
-            if file.metadata()?.len() >= 16 * 1024 {
-                // If memory mapping encounters an error, fall back to normal reads.
-                if let Ok(mmap) = mmap_file(&file) {
-                    return Ok(Self::Mmap(io::Cursor::new(mmap)));
-                }
+            if let Some(mmap) = maybe_mmap_file(&mut file)? {
+                return Ok(Self::Mmap(io::Cursor::new(mmap)));
             }
         }
         Ok(Self::File(file))
@@ -142,27 +139,55 @@ fn copy_wide(mut reader: impl Read, hasher: &mut kangarootwelve_xkcp::Hasher) ->
     }
 }
 
-fn mmap_file(file: &File) -> Result<memmap::Mmap> {
-    let metadata = file.metadata()?;
-    let file_size = metadata.len();
-    if !metadata.is_file() {
-        bail!("not a real file")
-    } else if file_size > isize::max_value() as u64 {
-        // https://github.com/danburkert/memmap-rs/issues/69
-        bail!("too long to safely map")
-    } else if file_size == 0 {
-        // https://github.com/danburkert/memmap-rs/issues/72
-        bail!("cannot mmap empty file")
-    } else {
-        // Explicitly set the length of the memory map, so that filesystem
-        // changes can't race to violate the invariants we just checked.
-        let mmap = unsafe {
-            memmap::MmapOptions::new()
-                .len(file_size as usize)
-                .map(&file)?
-        };
-        Ok(mmap)
+// Try to `mmap` a file, unless it's short enough that ordinary reads are faster, currently 16 KiB.
+// Return `Ok(None)` if mapping fails or if we don't attempt it. Only return `Err` for unexpected
+// failures that could leave the `File` in a bad state.
+//
+// This function is copied from the `blake3` crate. See the comments there about safety.
+fn maybe_mmap_file(file: &mut File) -> io::Result<Option<memmap2::Mmap>> {
+    // Seeking is more reliable than `.metadata()` for getting the length. Either is valid for
+    // regular files, but block devices sometimes report zero length despite being mappable. See
+    // https://github.com/BLAKE3-team/BLAKE3/pull/487.
+    use io::Seek;
+    // In debug mode only, check that the cursor is at the beginning. If the seek below fails,
+    // we'll leave the cursor where it is, and that can be confusing if it isn't at the beginning.
+    // (This function isn't public, but this comes up in test cases.)
+    if cfg!(debug_assertions) {
+        if let Ok(position) = file.stream_position() {
+            assert_eq!(position, 0, "initial file offset isn't at the beginning");
+        }
     }
+    // Seek to "16 KiB less 1 byte" from end-of-file. For short files, this will generally fail
+    // with an invalid argument error and leave the cursor at the beginning. For a regular file of
+    // exactly 16383 bytes, it will succeed and return 0, again leaving the cursor at the
+    // beginning. For some special/device files like /dev/random, it will also return 0. In all
+    // those cases, we can return without doing any extra syscalls to reset the cursor, and let the
+    // caller fall back to ordinary reads.
+    let seek_offset = MINIMUM_MMAP_SIZE - 1;
+    let seek_target = io::SeekFrom::End(-(seek_offset as i64));
+    let Ok(offset_len) = file.seek(seek_target) else {
+        return Ok(None); // short or unseekable files
+    };
+    if offset_len == 0 {
+        return Ok(None); // either exactly 16383 bytes, or e.g. /dev/random
+    }
+    // It's UB to produce a slice longer than `isize::MAX`. `memmap2` checks that internally, so
+    // it's kind of redundant to check it here, but we need to guard the `usize` cast in any case.
+    if offset_len <= isize::MAX as u64 - seek_offset {
+        // We have a seekable file that's long enough to be worth mapping, but not too long to map
+        // (e.g. on a 32-bit system). Try to map it. If this succeeds, we'll assume the caller
+        // isn't going to do any ordinary reads, so we don't need to `rewind`.
+        let mut mmap_options = memmap2::MmapOptions::new();
+        mmap_options.len((offset_len + seek_offset) as usize); // checked above
+        if let Ok(mmap) = unsafe { mmap_options.map(&*file) } {
+            return Ok(Some(mmap));
+        }
+    }
+    // `mmap` failed (or would've failed), so we need to `rewind` and let the caller do ordinary
+    // reads. This is the only failure case where we return `Err`, since if it does fail somehow
+    // (not clear if that's possible in practice), the file cursor position will be wrong.
+    file.rewind()?;
+    Ok(None)
 }
 
 fn write_hex_output(mut output: kangarootwelve_xkcp::OutputReader, args: &Args) -> Result<()> {
@@ -482,4 +507,101 @@ fn main() -> Result<()> {
         }
         std::process::exit(if files_failed > 0 { 1 } else { 0 });
     })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io;
+    use std::io::prelude::*;
+
+    #[test]
+    fn test_maybe_mmap_small_files() -> io::Result<()> {
+        let test_cases = [0, 1, MINIMUM_MMAP_SIZE - 2, MINIMUM_MMAP_SIZE - 1];
+        for len in test_cases {
+            dbg!(len);
+            // Create a file smaller than 16 KiB. `maybe_mmap_file` should return `Ok(None)`.
+            let input = vec![0xa5; len as usize];
+            let mut f = tempfile::NamedTempFile::new()?;
+            f.write_all(&input)?;
+            f.flush()?;
+            // We have a debug assert that the initial file offset is 0.
+            f.rewind()?;
+            assert!(maybe_mmap_file(f.as_file_mut())?.is_none());
+            // Check that the file cursor remains at start-of-file.
+            assert_eq!(f.stream_position()?, 0);
+            let mut hasher = kangarootwelve_xkcp::Hasher::new();
+            copy_wide(f.as_file_mut(), &mut hasher)?;
+            assert_eq!(kangarootwelve_xkcp::hash(&input), hasher.finalize());
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_maybe_mmap_mappable_files() -> io::Result<()> {
+        let test_cases = [MINIMUM_MMAP_SIZE, MINIMUM_MMAP_SIZE + 1];
+        for len in test_cases {
+            dbg!(len);
+            // Create a file that's 16 KiB or larger. `maybe_mmap_file` should return `Ok(Some(_))`.
+            let input = vec![0xa5; len as usize];
+            let mut f = tempfile::NamedTempFile::new()?;
+            f.write_all(&input)?;
+            f.flush()?;
+            // We have a debug assert that the initial file offset is 0.
+            f.rewind()?;
+            let Ok(Some(mmap)) = maybe_mmap_file(f.as_file_mut()) else {
+                panic!("mmap failed");
+            };
+            // `maybe_mmap_file` doesn't `rewind` the file in this case, so we need to do that
+            // ourselves.
+            assert_ne!(f.stream_position()?, 0);
+            f.rewind()?;
+            assert_eq!(mmap[..], input[..]);
+            assert_eq!(
+                kangarootwelve_xkcp::hash(&input),
+                kangarootwelve_xkcp::hash(&mmap),
+            );
+            let mut hasher = kangarootwelve_xkcp::Hasher::new();
+            copy_wide(f.as_file_mut(), &mut hasher)?;
+            assert_eq!(kangarootwelve_xkcp::hash(&input), hasher.finalize());
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn test_maybe_mmap_current_exe() -> io::Result<()> {
+        // The current executable should always be a regular file larger than 16 KiB, so mmap
+        // should ~always succeed. (A filesystem might not support mmap at all, but we don't test
+        // any of those in CI.)
+        let mut exe_file = File::open(std::env::current_exe()?)?;
+        assert!(exe_file.metadata()?.len() > MINIMUM_MMAP_SIZE);
+        let mmap = maybe_mmap_file(&mut exe_file)?.expect("maybe_mmap_file should return Some");
+        // Mainly we're testing that we got `Some` above, but go ahead and read the mmap just to
+        // make sure it doesn't bus fault or anything like that. `maybe_mmap_file` doesn't `rewind`
+        // the file in this case, so we need to do that ourselves.
+        exe_file.rewind()?;
+        let mut hasher = kangarootwelve_xkcp::Hasher::new();
+        copy_wide(&exe_file, &mut hasher)?;
+        assert_eq!(kangarootwelve_xkcp::hash(&mmap), hasher.finalize());
+        Ok(())
+    }
+
+    #[cfg(target_os = "linux")]
+    #[test]
+    fn test_unmappable_linux() -> io::Result<()> {
+        // I'm not aware of any similarly unmappable paths on macOS or Windows, so this test is
+        // Linux-only for now.
+        let unmappable_path = "/sys/kernel/btf/vmlinux";
+        let mut unmappable_file = File::open(unmappable_path)?;
+        // The file is large enough to attempt mmapping.
+        assert!(unmappable_file.metadata()?.len() > MINIMUM_MMAP_SIZE);
+        // We're allowed to read the file.
+        assert_eq!(unmappable_file.read(&mut [0])?, 1);
+        // But mmapping the file fails. (We have a debug assert that requires `rewind` here.)
+        unmappable_file.rewind()?;
+        unsafe { memmap2::Mmap::map(&unmappable_file) }.unwrap_err();
+        // `maybe_mmap_file` swallows that error and returns `None`.
+        assert!(maybe_mmap_file(&mut unmappable_file)?.is_none());
+        Ok(())
+    }
 }
--- k12sum/tests/test.rs
+++ k12sum/tests/test.rs
@@ -28,6 +28,18 @@ fn test_hash_one_raw() {
 }
 
 #[test]
+#[cfg(windows)]
+fn test_null_device_on_windows() {
+    let expected = hash(b"").to_hex();
+    let output = cmd!(k12sum_exe(), "--no-names", "NUL").read().unwrap();
+    assert_eq!(expected.as_str(), output);
+    let output = cmd!(k12sum_exe(), "--no-names", "--no-mmap", "NUL")
+        .read()
+        .unwrap();
+    assert_eq!(expected.as_str(), output);
+}
+
+#[test]
 fn test_hash_many() {
     let dir = tempfile::tempdir().unwrap();
     let file1 = dir.path().join("file1");
